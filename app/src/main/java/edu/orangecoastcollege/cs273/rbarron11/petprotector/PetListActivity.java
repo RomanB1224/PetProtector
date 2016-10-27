09519ed017b6eecec01672af2bd532a7aef56fa4
rbarron11 <roman.jedi@yahoo.com>
2016-10-27 15:13:13 -0700

    Successfully change image for pet

--- app/src/main/java/edu/orangecoastcollege/cs273/rbarron11/petprotector/PetListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/rbarron11/petprotector/PetListActivity.java
@@ -1,11 +1,16 @@
 package edu.orangecoastcollege.cs273.rbarron11.petprotector;
 
 import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.AnyRes;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -22,6 +27,7 @@ public class PetListActivity extends AppCompatActivity {
     //this member variable stores uri to whatever image has been selected
     //Default: none.png (R.drawable.none)
     private Uri imageURI;
+    private static final int REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +37,7 @@ public class PetListActivity extends AppCompatActivity {
         petImageView = (ImageView) findViewById(R.id.petImageView);
 
         //connects a full URI to any android resource (id, drawable, color, layout, ect)
-        //imageURI = getUriToResource(this, R.drawable.none);
+        imageURI = getUriResource(this, R.drawable.none);
 
         //Set the imageUri of the ImageView in code
         petImageView.setImageURI(imageURI);
@@ -65,8 +71,7 @@ public class PetListActivity extends AppCompatActivity {
             String[] perms = new String[permList.size()];
 
             //request permission from the user
-            int requestCode = 0;
-            ActivityCompat.requestPermissions(this, permList.toArray(perms), requestCode);
+            ActivityCompat.requestPermissions(this, permList.toArray(perms), REQUEST_CODE);
 
         }
 
@@ -76,7 +81,7 @@ public class PetListActivity extends AppCompatActivity {
         {
             //use intent to lauch the gallery and take pictures
             Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(galleryIntent, 0);
+            startActivityForResult(galleryIntent, REQUEST_CODE);
         }
         else
             Toast.makeText(this,
@@ -85,5 +90,42 @@ public class PetListActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //code to handle when the user closes the image gallery by selecting an image or pushing
+        // the back button
+
+        //the intent data is the URI selected from image gallery
+        //decide if user selected an image
+        if(data != null && requestCode == REQUEST_CODE && resultCode == RESULT_OK)
+        {
+            //set the imageUri to the data
+            imageURI = data.getData();
+            petImageView.setImageURI(imageURI);
+        }
+    }
+
+    /**
+     * Get URI to any resource type within  an android studio project static
+     * to allow other classes to use  it as a helper function
+     * @param context The current context
+     * @param resId The resource by given id
+     * @return Uri to resource by given id
+     * @throws Resources.NotFoundException if the given resource does not exist
+     */
+
+    public static Uri getUriResource(@NonNull Context context, @AnyRes int resId) throws Resources.NotFoundException
+    {
+        //Return  a resource instance for your application package
+        Resources res = context.getResources();
+
+        //return uri
+        return  Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE +
+        "://" + res.getResourcePackageName(resId) + '/' + res.getResourceTypeName(resId)
+                + '/' + res.getResourceEntryName(resId));
+
+    }
 
 }
